461b65224bca58fbce53040ec09f02590e18524a
Dak <vectorskeleton@gmail.com>
2020-12-13 17:23:34 -0800

    Initial commit

---
+++ DataDumper/DataDumperMain.cs
@@ -0,0 +1,128 @@
+﻿using Harmony;
+using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataDumper
+{
+    public class DataDumperMain : MelonMod
+    {
+        public int GAME_VERSION;
+        public static Dictionary<int, string> gameDataLookup;
+        //hardcoded
+        public static string GameDataSavePath = Path.Combine(MelonLoaderBase.UserDataPath, "GameData");
+
+
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Log("Reading game version...");
+            
+            if (!Directory.Exists(GameDataSavePath))
+            {
+                Directory.CreateDirectory(GameDataSavePath);
+            }
+
+            //This is really backwards because I was getting werid crashes when just trying to read the text
+            //and couldn't be bothered to figure out why
+            string gameVersionPath = Path.Combine(Imports.GetGameDirectory(), "revision.txt");
+            string gameVersion = File.ReadAllText(gameVersionPath);
+            int.TryParse(gameVersion, out int result);
+            GAME_VERSION = 21848;
+            MelonLogger.Log($"Game Version: {GAME_VERSION}");
+
+
+            var harmony = HarmonyInstance.Create("com.dakkhuza.offshoot");
+            harmony.PatchAll();
+            /*
+            *  Hash local game data for comparing
+            */
+            //Create gamedata lookup
+            MelonLogger.Log("Hashing GameData...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            gameDataLookup = new Dictionary<int, string>();
+            ResourceSet gameData = GameData.ResourceManager.GetResourceSet(CultureInfo.InvariantCulture, true, true);
+            IDictionaryEnumerator gameDataCollection = gameData.GetEnumerator();
+
+            while (gameDataCollection.MoveNext())
+            {
+                byte[] byteKey = gameDataCollection.Value as byte[];
+                string key = Encoding.UTF8.GetString(byteKey);
+                int hash = key.GetHashCode();
+                gameDataLookup.Add(hash, gameDataCollection.Key as string);
+            }
+            sw.Stop();
+            MelonLogger.Log("Hash done!");
+            MelonLogger.Log("Time elapsed: " + sw.Elapsed);
+        }
+
+        [HarmonyPatch(typeof(BinaryEncoder), "Decode")]
+        class Patch_BinaryDecoder
+        {
+            public static void Postfix(ref string __result)
+            {
+                //Ensure the file is game data related
+                if (__result.Contains("Headers"))
+                {
+                    int hash = __result.GetHashCode();
+                    gameDataLookup.TryGetValue(hash, out string name);
+
+                    if (name != null)
+                    {
+                        MelonLogger.Log("Found " + name);
+                        try
+                        {
+                            string filePath = Path.Combine(GameDataSavePath, name + ".json");
+                            if (File.Exists(filePath))
+                            {
+                                MelonLogger.Log("Reading [" + name + "] from disk...");
+                                __result = File.ReadAllText(filePath);
+                                return;
+                            }
+                            else
+                            {
+                                MelonLogger.Log("No file found at [" + filePath + "], writing file to disk...");
+                                File.WriteAllText(filePath, __result);
+                            }
+                        }
+                        catch
+                        {
+                            MelonLogger.LogError("Failed to write " + name + " to disk!!");
+                        }
+                    }
+                    else
+                    {
+                        string errorPath = Path.Combine(GameDataSavePath, "UNKNOWN");
+                        if (!Directory.Exists(errorPath))
+                        {
+                            Directory.CreateDirectory(errorPath);
+                        }
+                        string errorFilePath = Path.Combine(errorPath, hash + ".json");
+                        MelonLogger.LogError("Failed to find match for hash [" + hash + "]!");
+                        if (File.Exists(errorFilePath))
+                        {
+                            MelonLogger.LogWarning("-- FILE FOUND IN DUMP FOLDER WITH MATCHING HASH FILE NAME, LOADING INSTEAD --");
+                            __result = File.ReadAllText(errorFilePath);
+                            return;
+                        }
+
+                        MelonLogger.LogError("----- FILE CONTENT DUMP START -----");
+                        MelonLogger.LogError(__result);
+                        MelonLogger.LogError("----- FILE CONTENT DUMP END -----");
+                        MelonLogger.LogError("DUMPING FILE CONTENTS TO [" + errorFilePath + "]");
+                        File.WriteAllText(errorFilePath, __result);
+                    }
+                }
+            }
+        }
+    }
+}
