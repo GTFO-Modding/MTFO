1f31f565d60eac823f23270c18b2ef18b76c66c6
Dak <vectorskeleton@gmail.com>
2021-02-04 12:49:04 -0800

    Fixed the resource file not loading correctly

--- MTFO/ConfigStrings.Designer.cs
+++ MTFO/ConfigStrings.Designer.cs
@@ -39,7 +39,7 @@ namespace MTFO {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DataDumper.ConfigStrings", typeof(ConfigStrings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MTFO.ConfigStrings", typeof(ConfigStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
