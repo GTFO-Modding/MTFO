4b3d19177f35a9cd1c89d1c825a77cb8f81247d6
peelz <peelz.dev@gmail.com>
2021-02-24 23:38:20 -0500

    Migrate to the new .NET SDK csproj format

--- MTFO/MTFO.cs
+++ MTFO/MTFO.cs
@@ -16,7 +16,7 @@ namespace MTFO
             MODNAME = "MTFO",
             AUTHOR = "dak",
             GUID = "com." + AUTHOR + "." + MODNAME,
-            VERSION = "4.0.1";
+            VERSION = VersionInfo.Version;
 
 
         public override void Load()
