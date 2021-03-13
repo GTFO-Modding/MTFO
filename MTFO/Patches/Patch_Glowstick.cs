f35d2af13759caeb4698aa9f673ca868dff81279
Dak <vectorskeleton@gmail.com>
2021-03-12 21:26:27 -0800

    Added some null checks to glowsticks

--- MTFO/Patches/Patch_Glowstick.cs
+++ MTFO/Patches/Patch_Glowstick.cs
@@ -12,14 +12,18 @@ namespace MTFO.Patches
         //but im lazy
         public static void Postfix(ref GlowstickInstance __instance)
         {
-            if (ConfigManager.CustomContent.GlowstickHolder == null) return; 
+            if (!ConfigManager.HasCustomContent) return;
+            if (ConfigManager.CustomContent.GlowstickHolder == null) return;
             if (__instance == null) return;
             if (__instance.m_state == eFadeState.FadeOut || __instance.m_state == eFadeState.Done) return;
             if (!ConfigManager.CustomContent.GlowstickHolder.GlowstickLookup.TryGetValue(__instance.PublicName, out CustomGlowstick customGlowstick)) return;
 
-            __instance.m_light.SetRange(customGlowstick.Range);
-            __instance.m_light.SetColor(customGlowstick.Color * __instance.m_progression);
-            __instance.m_light.UpdateData();
+            if (__instance.m_hasLight)
+            {
+                __instance.m_light.SetRange(customGlowstick.Range);
+                __instance.m_light.SetColor(customGlowstick.Color * __instance.m_progression);
+                __instance.m_light.UpdateData();
+            }
         }
     }
 }
