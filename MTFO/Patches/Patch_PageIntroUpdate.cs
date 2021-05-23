c10606815a54e46de506ea1885845f4b0f78262b
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-05-23 10:00:12 +0000

    Add GUI patches
    Replace watermark, discord button, report bug buttons
    Remove matchmake buttons

---
+++ MTFO/Patches/Patch_PageIntroUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using CellMenu;
+
+namespace MTFO.Patches
+{
+    [HarmonyPatch(typeof(CM_PageIntro), "Update")]
+    class Patch_PageIntroUpdate
+    {
+        public static void Postfix(ref CM_PageIntro __instance)
+        {
+            //Replace report bug button text and link
+            __instance.m_startupScreen.m_btnBug.SetText("MTFO Wiki");
+            __instance.m_startupScreen.m_btnBug.OnBtnPressCallback = (id) => Application.OpenURL("https://wiki.mtfo.dev/");
+
+            //Replace discord button text and link
+            __instance.m_startupScreen.m_btnDiscord.SetText("Mod Server");
+            __instance.m_startupScreen.m_btnDiscord.OnBtnPressCallback = (id) => Application.OpenURL("https://discord.com/invite/rRMPtv4FAh");
+        }
+    }
+}
---
+++ MTFO/Patches/Patch_PageLoadoutUpdatePageData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using CellMenu;
+using TMPro;
+
+namespace MTFO.Patches
+{
+    [HarmonyPatch(typeof(CM_PageLoadout), "UpdatePageData")]
+    class Patch_PageLoadoutUpdatePageData
+    {
+        public static void Prefix(ref CM_PageLoadout __instance)
+        {
+            //Replace text blurb on the top right in lobby
+            __instance.m_movingContentHolder.Find("ShareServerId/ShareText").gameObject.GetComponent<TextMeshPro>()
+                .SetText("Do not play modded content on the official GTFO server or online matchmake lobbies.\n\nFeel free to join the unofficial discord server linked below and ask people to play.");
+
+            //Replace discord button text and link
+            __instance.m_discordButton.SetText("Mod Server");
+            __instance.m_discordButton.OnBtnPressCallback = (id) => Application.OpenURL("https://discord.com/invite/rRMPtv4FAh");
+        }
+    }
+}
