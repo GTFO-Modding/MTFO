78d9f7a8dc30c9b10d9d5929de357d319457d08c
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-10-14 17:38:04 -0500

    Make Json non-static

--- MTFO/Utilities/Json.cs
+++ MTFO/Utilities/Json.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using Native = System.Text.Json.JsonSerializer;
 
 namespace MTFO.Utilities
 {
-    public static class Json
+    public class Json
     {
+        public Json()
+        {
+            Options = new()
+            {
+                AllowTrailingCommas = true,
+                IncludeFields = true,
+                PropertyNameCaseInsensitive = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                WriteIndented = true
+            };
+        }
+
+        public Json(
+            bool allowTrailingCommas = true,
+            bool includeFields = true,
+            bool propertyNameCaseInsensitive = true,
+            bool writeIndented = true)
+        {
+            Options = new()
+            {
+                AllowTrailingCommas = allowTrailingCommas,
+                IncludeFields = includeFields,
+                PropertyNameCaseInsensitive = propertyNameCaseInsensitive,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                WriteIndented = writeIndented
+            };
+        }
         /// <summary>
         /// Checks if a file exists and will deserialize it using the given type, otherwise it will construct the output and write it to a file
         /// </summary>
-        public static void TryRead<T>(string path, out T output) where T : new()
+        public void TryRead<T>(string path, out T output) where T : new()
         {
             string json;
             if (File.Exists(path))
@@ -25,22 +53,16 @@ namespace MTFO.Utilities
             }
         }
 
-        public static string Serialize(object value)
+        public string Serialize(object value)
         {
-            return JsonSerializer.Serialize(value, options);
+            return Native.Serialize(value, Options);
         }
 
-        public static T Deserialize<T>(string json)
+        public T Deserialize<T>(string json)
         {
-            return JsonSerializer.Deserialize<T>(json, options);
+            return Native.Deserialize<T>(json, Options);
         }
 
-        private static JsonSerializerOptions options = new() {
-            AllowTrailingCommas = true,
-            IncludeFields = true,
-            PropertyNameCaseInsensitive = true,
-            ReadCommentHandling = JsonCommentHandling.Skip,
-            WriteIndented = true
-        };
+        public JsonSerializerOptions Options;
     }
 }
