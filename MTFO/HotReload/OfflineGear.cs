8eea6dfc657e39abbcb126e691fffa6ed08ad8c3
Dexusan <24143107+Dexusan@users.noreply.github.com>
2021-10-20 16:25:24 -0500

    Alphabetize order of HotReload.OfflineGear methods

--- MTFO/HotReload/OfflineGear.cs
+++ MTFO/HotReload/OfflineGear.cs
@@ -25,17 +25,28 @@ namespace MTFO.HotReload
             }
         }
 
-        private bool TryParseGearJson(string gearJson, out GearIDRange gearIDRange)
+        public static void Load(PlayerOfflineGearDataBlock block)
         {
-            if (string.IsNullOrEmpty(gearJson))
+            if (TryParse(block, out var gear)
+            &&  TryStash(gear))
             {
-                gearIDRange = null;
-                Log.Warn("Unable to assign GearIDRange due to null or empty GearJson");
-                return false;
+                Log.Verbose($"Loaded offline gear [{block.persistentID}] {block.name}");
             }
-            gearIDRange = new GearIDRange(gearJson);
-            if (gearIDRange == null) return false;
-            return true;
+        }
+
+        private static bool TryParse(PlayerOfflineGearDataBlock block, out OfflineGear result)
+        {
+            switch (block.Type)
+            {
+                case eOfflineGearType.StandardInventory:
+                case eOfflineGearType.RundownSpecificInventory:
+                    result = new OfflineGear(block);
+                    return true;
+                default:
+                    result = null;
+                    Log.Warn($"Unimplemented Offline Gear Type [{block.persistentID}] {block.Type}");
+                    return false;
+            }
         }
 
         private bool TryParseGearID(GearIDRange gearIDRange, out ItemDataBlock itemData, out int inventorySlot)
@@ -64,28 +75,17 @@ namespace MTFO.HotReload
             return true;
         }
 
-        public static void Load(PlayerOfflineGearDataBlock block)
+        private bool TryParseGearJson(string gearJson, out GearIDRange gearIDRange)
         {
-            if (TryParse(block, out var gear)
-            &&  TryStash(gear))
+            if (string.IsNullOrEmpty(gearJson))
             {
-                Log.Verbose($"Loaded offline gear [{block.persistentID}] {block.name}");
+                gearIDRange = null;
+                Log.Warn("Unable to assign GearIDRange due to null or empty GearJson");
+                return false;
             }
-        }
-
-        private static bool TryParse(PlayerOfflineGearDataBlock block, out OfflineGear result)
-        {
-            switch (block.Type)
-            {
-                case eOfflineGearType.StandardInventory:
-                case eOfflineGearType.RundownSpecificInventory:
-                    result = new OfflineGear(block);
-                    return true;
-                default:
-                    result = null;
-                    Log.Warn($"Unimplemented Offline Gear Type [{block.persistentID}] {block.Type}");
-                    return false;
-            }
+            gearIDRange = new GearIDRange(gearJson);
+            if (gearIDRange == null) return false;
+            return true;
         }
 
         private static bool TryStash(OfflineGear gear)
