925771505d0e6e3e976abcb6d849f48cd28b5875
Dak <vectorskeleton@gmail.com>
2022-07-19 08:14:28 -0700

    Update config string

--- MTFO/ConfigStrings.Designer.cs
+++ MTFO/ConfigStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace MTFO {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ConfigStrings {
@@ -97,7 +97,7 @@ namespace MTFO {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Dump GameData to BepInEx/gamedata/revision path?.
+        ///   Looks up a localized string similar to Dump GameData to BepInEx/GameData-Dump/revision path?.
         /// </summary>
         internal static string SETTING_DUMPDATA_DESC {
             get {
